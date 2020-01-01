14a8e8fcb75e778c484403cc77f2b50466ef948e
QiweiBao <baoqiwei1993@gmail.com>
2019-12-31 23:38:51 -0800

    initial commit

---
+++ client/js/scripts.js
@@ -0,0 +1,5 @@
+function getAlerted(){
+	var person1 = {name:'Eldor', age: 28, location: 'San Francisco'};
+	var person2 = {name:'Megan', age: 20, location: 'San Jose'};
+	document.getElementById('demo').innerHTML = person2.name;
+}
